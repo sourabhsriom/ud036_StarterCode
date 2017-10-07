f53574d6153e360f835d6faeb58135d200ebd676
sourabhsriom <sourabh.iitg@gmail.com>
2017-10-06 18:29:22 -0700

    another review change

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,7 +4,7 @@ import fresh_tomatoes as ft
 
 
 def __main__():
-    
+    #links for poster URLs
     lak = "https://upload.wikimedia.org/wikipedia/en/6/66/Lakshya.jpg"
     lag = "https://upload.wikimedia.org/wikipedia/en/b/b6/Lagaan.jpg"
     rdb = "https://upload.wikimedia.org/wikipedia/en/5/5f/RDB_poster.jpg"
@@ -15,18 +15,29 @@ def __main__():
     aw = "https://upload.wikimedia.org/wikipedia/en/7/77/A_Wednesday_Poster.JPG"  # NOQA
     g = "https://upload.wikimedia.org/wikipedia/en/6/6a/Gangs_of_Wasseypur_poster.jpg"  # NOQA
     
+    # links for trailers 
+    lak_link = "https://www.youtube.com/watch?v=YoKGmYyljmc"
+    lag_link = "https://www.youtube.com/watch?v=oSIGQ0YkFxs"
+    rdb_link = "https://www.youtube.com/watch?v=jW0Io8yB638"
+    lbs_link = "https://www.youtube.com/watch?v=CY37_al1IRs"
+    mi_link = "https://www.youtube.com/watch?v=G-Qeug86sQc"
+    nd_link = "https://www.youtube.com/watch?v=EK8XSmikhtg"
+    k_link = "https://www.youtube.com/watch?v=j1wEeuAosNM"
+    aw_link = "https://www.youtube.com/watch?v=u7RmUrMo770"
+    g_link = "https://www.youtube.com/watch?v=j-AkWDkXcMY"
+    
     # declare a list for movies and add all the movies you want to display
     movies = []
     
-    movies.append(media.movie("Lakshya", lak, "https://www.youtube.com/watch?v=YoKGmYyljmc"))  # NOQA
-    movies.append(media.movie("Lagaan", lag, "https://www.youtube.com/watch?v=oSIGQ0YkFxs"))  # NOQA
-    movies.append(media.movie("Rang De Basanti", rdb, "https://www.youtube.com/watch?v=jW0Io8yB638"))  # NOQA
-    movies.append(media.movie("Legend of Bhagat Singh", lbs, "https://www.youtube.com/watch?v=CY37_al1IRs"))  # NOQA
-    movies.append(media.movie("Mr. India", mi, "https://www.youtube.com/watch?v=G-Qeug86sQc"))  # NOQA
-    movies.append(media.movie("Naya Daur", nd, "https://www.youtube.com/watch?v=EK8XSmikhtg"))  # NOQA
-    movies.append(media.movie("Kahaani", k, "https://www.youtube.com/watch?v=j1wEeuAosNM"))  # NOQA
-    movies.append(media.movie("A Wednesday!", aw, "https://www.youtube.com/watch?v=u7RmUrMo770"))  # NOQA
-    movies.append(media.movie("Gangs of Wasseypur", g, "https://www.youtube.com/watch?v=j-AkWDkXcMY"))  # NOQA
+    movies.append(media.movie("Lakshya", lak, lak_link))
+    movies.append(media.movie("Lagaan", lag, lag_link))
+    movies.append(media.movie("Rang De Basanti", rdb, rdb_link))
+    movies.append(media.movie("Legend of Bhagat Singh", lbs, lbs_link)) 
+    movies.append(media.movie("Mr. India", mi, mi_link))
+    movies.append(media.movie("Naya Daur", nd, nd_link))
+    movies.append(media.movie("Kahaani", k, k_link))
+    movies.append(media.movie("A Wednesday!", aw, aw_link))
+    movies.append(media.movie("Gangs of Wasseypur", g, g_link))
    
     # sort the movie list by title
     movies.sort(key=operator.attrgetter('title'))    
@@ -34,4 +45,3 @@ def __main__():
     
 
 __main__()
-        
--- media.py
+++ media.py
@@ -1,10 +1,8 @@
 class movie:
     """Movie class defined to enable adding movie objects to a known data 
-       structure"""
-    
+       structure. The class has the attributes title, a URL for its poster
+       and a link for its trailer on youtube"""
     def __init__(self, title, poster, link):
-        
         self.title = title
         self.poster_image_url = poster
         self.trailer_youtube_url = link
-        
